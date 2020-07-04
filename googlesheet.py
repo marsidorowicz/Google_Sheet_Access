d644f152b7ba37cacbafd60a5a88cfd71324f065
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-07-04 09:22:03 +0200

    0.02v new json key

--- googlesheet.py
+++ googlesheet.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://googleapis.com/auth/drive']
 
-cred = ServiceAccountCredentials.from_json_keyfile_name('E:\\Programowanie\sigma-nimbus-278206-ac307a368b78.json')
+cred = ServiceAccountCredentials.from_json_keyfile_name('E:\\Programowanie\sigma-nimbus-278206-2a5d066f5e22.json')
 
 gs = gspread.authorize(cred)
 
